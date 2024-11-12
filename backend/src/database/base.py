73ecb199683f2038bd5993b6e636dc082704d1b8
ark948 <armankarimi948@gmail.com>
2024-11-12 21:54:56 +0330

    Added alembic database migration

--- backend/src/database/base.py
+++ backend/src/database/base.py
@@ -6,10 +6,3 @@ from sqlalchemy import Column, String, Integer
 
 class Base(DeclarativeBase):
     pass
-
-
-
-class Message(Base):
-    __tablename__ = "messages"
-    id = Column(Integer, primary_key=True, unique=True)
-    msg = Column(String(10))
