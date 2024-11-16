2f8af0ea04e5b2d52df9c9674034aa7bc2a5aad1
ark948 <armankarimi948@gmail.com>
2024-11-16 20:28:02 +0330

    Added pyproject.toml

--- backend/tests/test_main_app.py
+++ backend/tests/test_main_app.py
@@ -1,7 +1,11 @@
+def test_main_app_index_route(client):
+    response = client.get('/')
+    data = response.json()
+    assert response.status_code == 200
+    assert data['loc'] == 'root'
 
 
-
-def test_main_app(client):
+def test_main_app_test_route(client):
     response = client.get('/test')
     data = response.json()
     assert response.status_code == 200
